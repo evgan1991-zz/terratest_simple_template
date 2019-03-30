e4f1a2159e37e69cf9dfa8fd23b6878553a7a4eb
Yauheni Anashkin <yauheni_anashkin@epam.com>
2019-03-31 01:00:32 +0300

    add examples for module

---
+++ examples/variables.tf
@@ -0,0 +1,13 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "example" {
+  description = "Example variable"
+  default     = "example"
+}
+
+variable "example2" {
+  description = "Example variable 2"
+  default     = "example2"
+}
